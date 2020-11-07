bbfc81069811ecb18bb2869a40b2f93c4ae84899
Zenon <zenon@nitrox.com.br>
2020-11-06 21:55:12 -0300

    OTP Factory

--- python/src/empresas/otp/otp_factory.py
+++ python/src/empresas/otp/otp_factory.py
@@ -1,7 +1,19 @@
+import pandas as pd
+
+
 class OtpFactory:
     def __init__(self):
         super().__init__()
 
+    def read_from_excel(self):
+        path = '../../dados/demonstracoes_financeiras/OTP_demonstracoes_financeiras.xlsx'
+        df = pd.read_excel(open(path, 'rb'),
+                           sheet_name='anuais',
+                           header=None,
+                           index_col=None,
+                           usecols='A:L')
+        print(df)
+
     def execute(self):
         print('execute')
-        
+        self.read_from_excel()
